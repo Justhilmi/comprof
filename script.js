9e090af00afa2a06fefbf05597b239dc08835461
Justhilmi <hilmitrust@gmail.com>
2024-02-19 13:35:48 +0700

    script menghitung grade siswa

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-alert("Aplikasi Cek Nilai");
+alert("Aplikasi Cek Nilai")
 
-const nama  =prompt("Masukkan nilai anda "); 
-const nilai =prompt("Nilai :");
+const nama = prompt("Masukkan nama siswa");
+const nilai = prompt("Masukkan nilai siswa");
 
-if (nilai >= 90 && nilai <=100) {
-    alert(`Siswa ${nama} dengan nilai ${nilai} mendapat grade A`);
+// alert("Nama " + nama + " nilai " + nilai);
+// alert('Nama ${nama} nilai ${nilai}');
+
+if(nilai >= 90 && nilai <= 100) {
+    alert(`Siswa ${nama} dengan nilai ${nilai} mendapat A`);
 }
-if (nilai >= 100) {
-    alert(`Nilai tidak valid`)
+
+if(nilai > 100) {
+    alert(`Nilai terlalu tinggi, harap masukkan dengan benar!`);
 }
-else if (nilai >= 80) {
-    alert(`Siswa ${nama} dengan nilai ${nilai} mendapat grade B`);
+
+else if (nilai >= 80 && nilai <= 89) {
+    alert(`Siswa ${nama} dengan nilai ${nilai} mendapat B`);
 }
-else if (nilai >= 70) {
-    alert(`Siswa ${nama} dengan nilai ${nilai} mendapat grade C`);
+
+else if (nilai >= 70 && nilai <= 79) {
+    alert(`Siswa ${nama} dengan nilai ${nilai} mendapat C`);
 }
-else if (nilai >= 60) {
-    alert(`Siswa ${nama} dengan nilai ${nilai} mendapat grade D`);
+
+else if (nilai >= 60 && nilai <= 69) {
+    alert(`Siswa ${nama} dengan nilai ${nilai} mendapat D`);
 }
+
 else {
-    alert(`Siswa ${nama} dengan nilai ${nilai} mendapat grade E`);
+    alert(`Siswa ${nama} dengan nilai ${nilai} mendapat E`);
 }
