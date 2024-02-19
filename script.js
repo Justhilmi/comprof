64756b05f611867b1259443432ee42b8a82f9309
Justhilmi <hilmitrust@gmail.com>
2024-02-19 13:26:17 +0700

    script menghitung grade siswa

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ const nama  =prompt("Masukkan nilai anda ");
 const nilai =prompt("Nilai :");
 
 if (nilai >= 90 && nilai <=100) {
-    alert("Siswa ${nama} dengan nilai ${nilai} mendapat grade A")
+    alert(`Siswa ${nama} dengan nilai ${nilai} mendapat grade A`)
 }
 if (nilai >= 100) {
-    alert("Nilai tidak valid")
+    alert(`Nilai tidak valid`)
 }
 else if (nilai >= 80) {
-    alert("Siswa ${nama} dengan nilai ${nilai} mendapat grade B")
+    alert(`Siswa ${nama} dengan nilai ${nilai} mendapat grade B`)
 }
 else if (nilai >= 70) {
-    alert("Siswa ${nama} dengan nilai ${nilai} mendapat grade C")
+    alert(`Siswa ${nama} dengan nilai ${nilai} mendapat grade C`)
 }
 else if (nilai >= 60) {
-    alert("Siswa ${nama} dengan nilai ${nilai} mendapat grade D")
+    alert(`Siswa ${nama} dengan nilai ${nilai} mendapat grade D`)
 }
 else {
-    alert("Siswa ${nama} dengan nilai ${nilai} mendapat grade E")
+    alert(`Siswa ${nama} dengan nilai ${nilai} mendapat grade E`)
 }
